bf5991ce157fab8d4ef9aaf667d227e3f415f420
glopes <glopesdev@users.noreply.github.com>
2016-10-19 20:38:02 +0100

    Updated SquareWave combinator to allow for infinite pulse trains.

--- Bonsai.Videogame/Bonsai.Videogame/SquareWave.cs
+++ Bonsai.Videogame/Bonsai.Videogame/SquareWave.cs
@@ -42,12 +42,15 @@ namespace Bonsai.Videogame
             set { Period = XmlConvert.ToTimeSpan(value); }
         }
 
-        public int Count { get; set; }
+        public int? Count { get; set; }
 
         public override IObservable<int> Process<TSource>(IObservable<TSource> source)
         {
+            var count = Count;
+            var timer = Observable.Timer(TimeSpan.Zero, Period, HighResolutionScheduler.Default);
+            if (count.HasValue) timer = timer.Take(count.Value);
             return from evt in source
-                   from value in Observable.Timer(TimeSpan.Zero, Period, HighResolutionScheduler.Default).Take(Count)
+                   from value in timer
                    from wave in Observable.Repeat(Value, 1).Merge(Observable.Repeat(0, 1).Delay(Duration, HighResolutionScheduler.Default))
                    select wave;
         }
