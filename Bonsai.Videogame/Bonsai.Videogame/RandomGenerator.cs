8a3b10a87998ad549bf90f1b72912f52132b9591
glopes <glopesdev@users.noreply.github.com>
2016-10-24 15:59:04 +0100

    Added modules for random generation of stimulus position.

---
+++ Bonsai.Videogame/Bonsai.Videogame/RandomGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Videogame
+{
+    public class RandomGenerator : Source<Random>
+    {
+        public int? Seed { get; set; }
+
+        public override IObservable<Random> Generate()
+        {
+            return Observable.Defer(() =>
+            {
+                var seed = Seed;
+                var random = seed.HasValue ? new Random(seed.Value) : new Random();
+                return Observable.Return(random);
+            });
+        }
+    }
+}
---
+++ Bonsai.Videogame/Bonsai.Videogame/RandomPosition.cs
@@ -0,0 +1,33 @@
+﻿using Ode.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Videogame
+{
+    public class RandomPosition : Combinator<Random, Vector3>
+    {
+        [TypeConverter(typeof(NumericAggregateConverter))]
+        public Vector3? Minimum { get; set; }
+
+        [TypeConverter(typeof(NumericAggregateConverter))]
+        public Vector3? Maximum { get; set; }
+
+        public override IObservable<Vector3> Process(IObservable<Random> source)
+        {
+            return source.Select(random =>
+            {
+                var minimum = Minimum.GetValueOrDefault(Vector3.Zero);
+                var maximum = Maximum.GetValueOrDefault(Vector3.One);
+                return new Vector3(
+                    minimum.X == maximum.X ? minimum.X : random.NextDouble() * (maximum.X - minimum.X) + minimum.X,
+                    minimum.Y == maximum.Y ? minimum.Y : random.NextDouble() * (maximum.Y - minimum.Y) + minimum.Y,
+                    minimum.Z == maximum.Z ? minimum.Z : random.NextDouble() * (maximum.Z - minimum.Z) + minimum.Z);
+            });
+        }
+    }
+}
