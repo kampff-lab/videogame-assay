5708f7cf6843edaa7071322aceb22f8749875d81
glopes <glopesdev@users.noreply.github.com>
2017-03-07 18:04:39 +0000

    Moved point distortion model to a separate node to allow raw data logging.

--- Bonsai.Videogame/Bonsai.Videogame/ColorLedTracking.cs
+++ Bonsai.Videogame/Bonsai.Videogame/ColorLedTracking.cs
@@ -20,22 +20,7 @@ namespace Bonsai.Videogame
         [Description("Threshold used to segment the blue LED.")]
         public int ThresholdBlue { get; set; }
 
-        [Category("Undistort")]
-        [TypeConverter(typeof(UnidimensionalArrayConverter))]
-        [Description("Linear coefficients used to undistort the X position.")]
-        public double[] UndistortX { get; set; }
 
-        [Category("Undistort")]
-        [TypeConverter(typeof(UnidimensionalArrayConverter))]
-        [Description("Linear coefficients used to undistort the Y position.")]
-        public double[] UndistortY { get; set; }
-
-        Point2f UndistortPoint(Point2f point, double[] xw, double[] yw)
-        {
-            var x = xw[0] + xw[1] * point.X + xw[2] * point.Y + xw[3] * point.X * point.X + xw[4] * point.Y * point.Y;
-            var y = yw[0] + yw[1] * point.X + yw[2] * point.Y + yw[3] * point.X * point.X + yw[4] * point.Y * point.Y;
-            return new Point2f((float)x, (float)-y);
-        }
 
         public override IObservable<Tuple<Point2f, Point2f>> Process(IObservable<IplImage> source)
         {
@@ -43,7 +28,6 @@ namespace Bonsai.Videogame
             {
                 var red = default(IplImage);
                 var blue = default(IplImage);
-                Point2f redpoint, bluepoint;
                 return source.Select(input =>
                 {
                     if (red == null) red = new IplImage(input.Size, input.Depth, 1);
@@ -53,9 +37,7 @@ namespace Bonsai.Videogame
                     CV.Threshold(blue, blue, ThresholdBlue, 255, ThresholdTypes.Binary);
                     var redled = ConnectedComponent.FromImage(red).Centroid;
                     var blueled = ConnectedComponent.FromImage(blue).Centroid;
-                    redpoint = UndistortPoint(redled, UndistortX, UndistortY);
-                    bluepoint = UndistortPoint(blueled, UndistortX, UndistortY);
-                    return Tuple.Create(redpoint, bluepoint);
+                    return Tuple.Create(redled, blueled);
                 });
             });
         }
---
+++ Bonsai.Videogame/Bonsai.Videogame/UndistortPoints.cs
@@ -0,0 +1,41 @@
+﻿using OpenCV.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Videogame
+{
+    public class UndistortPoints : Transform<Tuple<Point2f, Point2f>, Tuple<Point2f, Point2f>>
+    {
+        [Category("Undistort")]
+        [TypeConverter(typeof(UnidimensionalArrayConverter))]
+        [Description("Linear coefficients used to undistort the X position.")]
+        public double[] UndistortX { get; set; }
+
+        [Category("Undistort")]
+        [TypeConverter(typeof(UnidimensionalArrayConverter))]
+        [Description("Linear coefficients used to undistort the Y position.")]
+        public double[] UndistortY { get; set; }
+
+        Point2f UndistortPoint(Point2f point, double[] xw, double[] yw)
+        {
+            var x = xw[0] + xw[1] * point.X + xw[2] * point.Y + xw[3] * point.X * point.X + xw[4] * point.Y * point.Y;
+            var y = yw[0] + yw[1] * point.X + yw[2] * point.Y + yw[3] * point.X * point.X + yw[4] * point.Y * point.Y;
+            return new Point2f((float)x, (float)-y);
+        }
+
+        public override IObservable<Tuple<Point2f, Point2f>> Process(IObservable<Tuple<Point2f, Point2f>> source)
+        {
+            return source.Select(input =>
+            {
+                var point1 = UndistortPoint(input.Item1, UndistortX, UndistortY);
+                var point2 = UndistortPoint(input.Item2, UndistortX, UndistortY);
+                return Tuple.Create(point1, point2);
+            });
+        }
+    }
+}
