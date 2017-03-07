5a75f0888ec1ac4eeaec540cc2dfc89ad86236be
glopes <glopesdev@users.noreply.github.com>
2017-03-07 12:20:48 +0000

    Updated led tracking to extract undistorted centroid positions.

--- Bonsai.Videogame/Bonsai.Videogame/ColorLedTracking.cs
+++ Bonsai.Videogame/Bonsai.Videogame/ColorLedTracking.cs
@@ -2,6 +2,7 @@
 using OpenCV.Net;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
@@ -11,9 +12,30 @@ namespace Bonsai.Videogame
 {
     public class ColorLedTracking : Transform<IplImage, Tuple<Point2f, Point2f>>
     {
+        [Category("Threshold")]
+        [Description("Threshold used to segment the red LED.")]
         public int ThresholdRed { get; set; }
 
+        [Category("Threshold")]
+        [Description("Threshold used to segment the blue LED.")]
         public int ThresholdBlue { get; set; }
+
+        [Category("Undistort")]
+        [TypeConverter(typeof(UnidimensionalArrayConverter))]
+        [Description("Linear coefficients used to undistort the X position.")]
+        public double[] UndistortX { get; set; }
+
+        [Category("Undistort")]
+        [TypeConverter(typeof(UnidimensionalArrayConverter))]
+        [Description("Linear coefficients used to undistort the Y position.")]
+        public double[] UndistortY { get; set; }
+
+        Point2f UndistortPoint(Point2f point, double[] xw, double[] yw)
+        {
+            var x = xw[0] + xw[1] * point.X + xw[2] * point.Y + xw[3] * point.X * point.X + xw[4] * point.Y * point.Y;
+            var y = yw[0] + yw[1] * point.X + yw[2] * point.Y + yw[3] * point.X * point.X + yw[4] * point.Y * point.Y;
+            return new Point2f((float)x, (float)-y);
+        }
 
         public override IObservable<Tuple<Point2f, Point2f>> Process(IObservable<IplImage> source)
         {
@@ -21,6 +43,7 @@ namespace Bonsai.Videogame
             {
                 var red = default(IplImage);
                 var blue = default(IplImage);
+                Point2f redpoint, bluepoint;
                 return source.Select(input =>
                 {
                     if (red == null) red = new IplImage(input.Size, input.Depth, 1);
@@ -28,9 +51,11 @@ namespace Bonsai.Videogame
                     CV.Split(input, red, null, blue, null);
                     CV.Threshold(red, red, ThresholdRed, 255, ThresholdTypes.Binary);
                     CV.Threshold(blue, blue, ThresholdBlue, 255, ThresholdTypes.Binary);
-                    var redled = ConnectedComponent.FromImage(red);
-                    var blueled = ConnectedComponent.FromImage(blue);
-                    return Tuple.Create(redled.Centroid, blueled.Centroid);
+                    var redled = ConnectedComponent.FromImage(red).Centroid;
+                    var blueled = ConnectedComponent.FromImage(blue).Centroid;
+                    redpoint = UndistortPoint(redled, UndistortX, UndistortY);
+                    bluepoint = UndistortPoint(blueled, UndistortX, UndistortY);
+                    return Tuple.Create(redpoint, bluepoint);
                 });
             });
         }
