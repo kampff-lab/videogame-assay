1a2e50209cbcf2622e6aa6cda924d72f44fd9006
glopes <glopesdev@users.noreply.github.com>
2017-03-27 21:05:28 +0100

    Added ShuffleNames transform for randomly generating trial blocks.

---
+++ Bonsai.Videogame/Bonsai.Videogame/ShuffleNames.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Videogame
+{
+    public class ShuffleNames : Transform<Random, IEnumerable<string>>
+    {
+        readonly Collection<Weighted<string>> names = new Collection<Weighted<string>>();
+
+        public int TrialCount { get; set; }
+
+        [TypeConverter(typeof(ExpandableCollectionConverter))]
+        public Collection<Weighted<string>> Names
+        {
+            get { return names; }
+        }
+
+        public override IObservable<IEnumerable<string>> Process(IObservable<Random> source)
+        {
+            return source.Select(input =>
+            {
+                var trialCount = TrialCount;
+                var trials = new List<string>(trialCount);
+                foreach (var name in names)
+                {
+                    var count = name.Weight * trialCount;
+                    for (int i = 0; i < count; i++)
+                    {
+                        trials.Add(name.Value);
+                    }
+                }
+
+                for (int i = 0; i < trials.Count; i++)
+                {
+                    var index = input.Next(i, trials.Count);
+                    var temp = trials[i];
+                    trials[i] = trials[index];
+                    trials[index] = temp;
+                }
+
+                return trials;
+            });
+        }
+
+
+    }
+}
---
+++ Bonsai.Videogame/Bonsai.Videogame/Weighted.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Videogame
+{
+    public class Weighted<T>
+    {
+        public Weighted()
+        {
+        }
+
+        public Weighted(T value, double weight)
+        {
+            Weight = weight;
+            Value = value;
+        }
+
+        public double Weight { get; set; }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Value, Weight);
+        }
+    }
+}
