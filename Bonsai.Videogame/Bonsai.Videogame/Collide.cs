20cd219143a8025b489cdad9b5756671f21ef898
glopes <glopesdev@users.noreply.github.com>
2017-03-27 21:07:13 +0100

    Added Collide module to support explicit computation of geom collisions.

---
+++ Bonsai.Videogame/Bonsai.Videogame/Collide.cs
@@ -0,0 +1,26 @@
+﻿using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Videogame
+{
+    public class Collide : Transform<Geom, ContactGeom[]>
+    {
+        public int MaxContacts { get; set; }
+
+        public override IObservable<ContactGeom[]> Process(IObservable<Geom> source)
+        {
+            return source.Select(input =>
+            {
+                var contacts = new ContactGeom[MaxContacts];
+                var ncontacts = Geom.Collide(input, input.Space, contacts);
+                Array.Resize(ref contacts, ncontacts);
+                return contacts;
+            });
+        }
+    }
+}
