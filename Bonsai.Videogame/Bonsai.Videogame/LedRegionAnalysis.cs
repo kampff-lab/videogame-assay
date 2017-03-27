5a15c344aa3f04c42d402389711514687c5b26fe
glopes <glopesdev@users.noreply.github.com>
2017-03-27 13:04:25 +0100

    Updated led analysis to allow keeping the latest valid value on failure.

--- Bonsai.Videogame/Bonsai.Videogame/LedRegionAnalysis.cs
+++ Bonsai.Videogame/Bonsai.Videogame/LedRegionAnalysis.cs
@@ -11,21 +11,34 @@ namespace Bonsai.Videogame
 {
     public class LedRegionAnalysis : Transform<Tuple<Point2f, Point2f>, ConnectedComponent>
     {
+        public bool KeepLatest { get; set; }
+
         public override IObservable<ConnectedComponent> Process(IObservable<Tuple<Point2f, Point2f>> source)
         {
-            return source.Select(input =>
+            return Observable.Defer(() =>
             {
-                var result = new ConnectedComponent();
-                var redled = input.Item1;
-                var blueled = input.Item2;
-                var direction = blueled - redled;
-                var r2 = direction.X * direction.X + direction.Y * direction.Y;
-                var r = Math.Sqrt(r2);
-                result.Centroid = redled;
-                result.Orientation = Math.Atan2(direction.Y, direction.X);
-                result.MajorAxisLength = result.MinorAxisLength = 2 * r;
-                result.Area = 2 * Math.PI * r2;
-                return result;
+                var latest = default(ConnectedComponent);
+                return source.Select(input =>
+                {
+                    var redled = input.Item1;
+                    var blueled = input.Item2;
+                    if ((float.IsNaN(redled.X) || float.IsNaN(redled.Y)) && latest != null)
+                    {
+                        return latest;
+                    }
+
+                    var result = new ConnectedComponent();
+                    var direction = blueled - redled;
+                    var r2 = direction.X * direction.X + direction.Y * direction.Y;
+                    var r = Math.Sqrt(r2);
+                    result.Centroid = redled;
+                    result.Orientation = Math.Atan2(direction.Y, direction.X);
+                    result.MajorAxisLength = result.MinorAxisLength = 2 * r;
+                    result.Area = 2 * Math.PI * r2;
+                    if (KeepLatest) latest = result;
+                    else latest = null;
+                    return result;
+                });
             });
         }
     }
