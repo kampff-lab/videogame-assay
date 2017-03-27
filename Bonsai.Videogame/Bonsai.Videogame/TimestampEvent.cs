e48cff4e32a845a410ed97ccd6343789853d211d
glopes <glopesdev@users.noreply.github.com>
2017-03-27 21:11:10 +0100

    Added TimestampEvent transform for supporting easier timestamped event logging.

---
+++ Bonsai.Videogame/Bonsai.Videogame/TimestampEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Videogame
+{
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class TimestampEvent : Combinator<TimestampedEvent>, INamedElement
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public override IObservable<TimestampedEvent> Process<TSource>(IObservable<TSource> source)
+        {
+            return source.Select(input => new TimestampedEvent(HighResolutionScheduler.Now, Name, Value));
+        }
+
+        public IObservable<TimestampedEvent> Process<TSource>(IObservable<Timestamped<TSource>> source)
+        {
+            return source.Select(input => new TimestampedEvent(input.Timestamp, Name, Value));
+        }
+    }
+}
---
+++ Bonsai.Videogame/Bonsai.Videogame/TimestampedEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Videogame
+{
+    public class TimestampedEvent
+    {
+        public TimestampedEvent(DateTimeOffset timestamp, string name, string value)
+        {
+            Timestamp = timestamp;
+            Name = name;
+            Value = value;
+        }
+
+        public DateTimeOffset Timestamp { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
